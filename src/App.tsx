4ddddf640c1cedc902a9629c8a2e36079dbe30e0
sadatdev <sadatjubayerdev@gmail.com>
2022-03-23 19:48:30 +0600

    📦 NEW: ESLint & Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-center p-12">
-      Hello world!
-    </h1>
-  );
+    return <h1 className="text-3xl font-bold underline text-center p-12">Hello world!</h1>;
 }
