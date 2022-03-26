765fe66457a2be08f69196c10f3b5bd0a72ad69c
sadatdev <sadatjubayerdev@gmail.com>
2022-03-26 10:27:25 +0600

    👌 IMPROVE: Routing

--- src/pages/admin/createQuiz.page.tsx
+++ src/pages/admin/createQuiz.page.tsx
@@ -1,5 +1,9 @@
 import { Layout } from 'components';
+import { useParams } from 'react-router-dom';
 
 export const CreateQUizPage = () => {
+    const prams = useParams();
+    console.log('prams', prams);
+
     return <Layout>Test page</Layout>;
 };
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,22 @@
-import { BrowserRouter } from 'react-router-dom';
-import { AdminRouter } from './admin.router';
-import { UserRouter } from './user.router';
+import { appRoutes } from 'constant';
+import { CreateQUizPage } from 'pages/admin/createQuiz.page';
+import DashboardPage from 'pages/admin/dashboard.page';
+import HomePage from 'pages/user/home.page';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export const Router = () => {
     return (
         <BrowserRouter>
-            <AdminRouter />
-            <UserRouter />
+            <Routes>
+                {/* Admin Routes */}
+                <Route path={appRoutes.ADMIN_DASHBOARD} element={<DashboardPage />} />
+                <Route path={appRoutes.QUIZ_EDITOR}>
+                    <Route path=":id" element={<CreateQUizPage />} />
+                </Route>
+
+                {/* User Routes */}
+                <Route path={appRoutes.USER_HOME} element={<HomePage />} />
+            </Routes>
         </BrowserRouter>
     );
 };
